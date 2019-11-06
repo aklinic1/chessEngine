161c815704af63381b56c9272e2f6d0653ffcc56
Amer Klinic <aklinic1@etf.unsa.ba>
2019-11-06 20:17:34 +0100

    implementirana klassa Knight + konsrtruktor i metoda checkMove

---
+++ src/ba/unsa/etf/rpr/Knight.java
@@ -0,0 +1,17 @@
+package ba.unsa.etf.rpr;
+
+public class Knight extends ChessPiece{
+    public Knight(String position, Color color) {
+        super(position, color);
+    }
+
+    @Override
+    public boolean checkMove(String postion){
+        int [] niz = setComparablePositionValues(this.position, position);
+        int pocetna1 = niz[0], pocetna2 = niz[1], krajnja1 = niz[2], krajnja2 = niz[3];
+        if((Math.abs(pocetna1 - krajnja1) == 2 && Math.abs(pocetna2 - krajnja2) == 1)
+                || (Math.abs(pocetna2 - krajnja2) == 2 && Math.abs(pocetna1 - krajnja1) == 1))
+            return true;
+        return false;
+    }
+}
