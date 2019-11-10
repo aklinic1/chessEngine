43163b6ab3d15984dfd29d3009a6a672b16ecab6
Amer Klinic <aklinic1@etf.unsa.ba>
2019-11-10 22:19:18 +0100

    dodan komentar

--- src/ba/unsa/etf/rpr/Pawn.java
+++ src/ba/unsa/etf/rpr/Pawn.java
@@ -10,6 +10,6 @@ public class Pawn extends ChessPiece {
         int pocetna1 = niz[0], pocetna2 = niz[1], krajnja1 = niz[2], krajnja2 = niz[3];
         if(pocetna1 == krajnja1 && (pocetna2 == krajnja2 - 1 || pocetna2 == krajnja2 + 1)) return true;
         if(Math.abs(pocetna1 - krajnja1) == 1 && Math.abs(pocetna2 - krajnja2)) return true; //moze se kretati jedno polje dijagonalno pod uslovom
-        return false;                                                                          //da je na toj poziciji figura druge boje
-    }
+        return false;                                                                          //da je na toj poziciji figura druge boje sto cemo provjeriti
+    }                                                                                               //u klasi board
 }
