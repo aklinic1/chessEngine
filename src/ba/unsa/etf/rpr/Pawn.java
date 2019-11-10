d375c482383e8ca17bb49b7d85961ad19aadfe0b
Amer Klinic <aklinic1@etf.unsa.ba>
2019-11-10 22:18:03 +0100

    dodan komentar - metoda move

--- src/ba/unsa/etf/rpr/Pawn.java
+++ src/ba/unsa/etf/rpr/Pawn.java
@@ -9,7 +9,7 @@ public class Pawn extends ChessPiece {
         int [] niz = setComparablePositionValues(this.position, position);
         int pocetna1 = niz[0], pocetna2 = niz[1], krajnja1 = niz[2], krajnja2 = niz[3];
         if(pocetna1 == krajnja1 && (pocetna2 == krajnja2 - 1 || pocetna2 == krajnja2 + 1)) return true;
-        if(Math.abs(pocetna1 - krajnja1) == 1 && Math.abs(pocetna2 - krajnja2)) return true;
-        return false;
+        if(Math.abs(pocetna1 - krajnja1) == 1 && Math.abs(pocetna2 - krajnja2)) return true; //moze se kretati jedno polje dijagonalno pod uslovom
+        return false;                                                                          //da je na toj poziciji figura druge boje
     }
 }
