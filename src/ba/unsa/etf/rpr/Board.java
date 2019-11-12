387cd862b7ad4bcb0362383ea09b874be4b7b864
Amer Klinic <aklinic1@etf.unsa.ba>
2019-11-12 19:45:44 +0100

    izmijenjene move metode, dodani uslovi za pomjeranje figura tipa Knight i Pawn

--- src/ba/unsa/etf/rpr/Board.java
+++ src/ba/unsa/etf/rpr/Board.java
@@ -8,6 +8,7 @@ public class Board {
     private ChessPiece [][]fields = new ChessPiece[8][8];
     private King blackKing, whiteKing;                  //posto su kraljevi najbitnije figure od cijeg ostojanja igra i zavisi
                                                                 //dodati cemo clanove koji ce u svakom trenutku znati njihovu poziciju;
+
     private boolean checkPosition(String position){  //provjerava da li je pozicija validna tj. unutar sahoveske table, da li je duzina stringa veca od 2
         if(position.length() > 2) return false;
         else if(position.charAt(0) < 'a' || position.charAt(0) > 'h') {
@@ -36,38 +37,37 @@ public class Board {
     public Board() {
         int i;
         for(i = 0; i < 8; i++){
-            fields[i][1] = new Pawn((char)('a' + i) + String.valueOf(1), ChessPiece.Color.White);
-            fields[i][6] = new Pawn((char)('a' + i) + String.valueOf(6), ChessPiece.Color.Black);
+            fields[i][1] = new Pawn((char)('a' + i) + String.valueOf(1), ChessPiece.Color.WHITE);
+            fields[i][6] = new Pawn((char)('a' + i) + String.valueOf(6), ChessPiece.Color.BLACK);
         }
-        fields[0][0] = new Rook("a1", ChessPiece.Color.White);
-        fields[7][0] = new Rook("a8", ChessPiece.Color.White);
-        fields[0][7] = new Rook("h1", ChessPiece.Color.Black);
-        fields[7][7] = new Rook("h8", ChessPiece.Color.Black);
-        fields[1][0] = new Knight("b1", ChessPiece.Color.Black);
-        fields[6][0] = new Knight("g1", ChessPiece.Color.Black);
-        fields[1][7] = new Knight("b7", ChessPiece.Color.White);
-        fields[6][7] = new Knight("g7", ChessPiece.Color.White);
-        fields[2][0] = new Bishop("c1", ChessPiece.Color.Black);
-        fields[5][0] = new Bishop("f1", ChessPiece.Color.Black);
-        fields[2][7] = new Bishop("c8", ChessPiece.Color.White);
-        fields[5][7] = new Bishop("f8", ChessPiece.Color.White);
-        fields[3][0] = new Queen("d1", ChessPiece.Color.White);
-        fields[4][0] = new King("e1", ChessPiece.Color.White);
+        fields[0][0] = new Rook("a1", ChessPiece.Color.WHITE);
+        fields[7][0] = new Rook("a8", ChessPiece.Color.WHITE);
+        fields[0][7] = new Rook("h1", ChessPiece.Color.BLACK);
+        fields[7][7] = new Rook("h8", ChessPiece.Color.BLACK);
+        fields[1][0] = new Knight("b1", ChessPiece.Color.BLACK);
+        fields[6][0] = new Knight("g1", ChessPiece.Color.BLACK);
+        fields[1][7] = new Knight("b7", ChessPiece.Color.WHITE);
+        fields[6][7] = new Knight("g7", ChessPiece.Color.WHITE);
+        fields[2][0] = new Bishop("c1", ChessPiece.Color.BLACK);
+        fields[5][0] = new Bishop("f1", ChessPiece.Color.BLACK);
+        fields[2][7] = new Bishop("c8", ChessPiece.Color.WHITE);
+        fields[5][7] = new Bishop("f8", ChessPiece.Color.WHITE);
+        fields[3][0] = new Queen("d1", ChessPiece.Color.WHITE);
+        fields[4][0] = new King("e1", ChessPiece.Color.WHITE);
         whiteKing = (King)fields[4][0];
-        fields[3][7] = new Queen("d8", ChessPiece.Color.Black);
-        fields[4][7] = new King("e8", ChessPiece.Color.Black);
+        fields[3][7] = new Queen("d8", ChessPiece.Color.BLACK);
+        fields[4][7] = new King("e8", ChessPiece.Color.BLACK);
         blackKing = (King)fields[4][7];
     }
 
-    public void move(Class type, ChessPiece.Color color, String position) throws IllegalChessMoveException {
+    public void move(Class type, ChessPiece.Color color, String position) throws Exception {
         if(!checkPosition(position)) throw new IllegalChessMoveException("Ilegalan potez");
         int i,j;
         for(i = 0; i < 8; i++) {
             for (j = 0; j < 8; j++) {
                 if (type.isInstance(fields[i][j]) && color == fields[i][j].getColor()){
                     if(fields[i][j].getPosition() == position) throw new IllegalChessMoveException("Ilegalan potez");
-                    if(checkPathToNewPosition(fields[i][j].getPosition(), position))
-                        fields[i][j].move(position);
+                        move(fields[i][j].getPosition(), position);
                 }
             }
         }
@@ -77,12 +77,27 @@ public class Board {
 
         int[] pozicija = getMatrixPosition(oldPosition);
         int i = pozicija[0], j = pozicija[1];
-        if (checkPosition(oldPosition) || fields[i][j] == null)  //provjerava da li je uopste unesena validna pozcicija te da li na poziciji ima figura
+        int[] pozicija1 = getMatrixPosition(newPosition);
+        int k = pozicija1[0], l = pozicija1[1];
+        if (checkPosition(newPosition) || fields[i][j] == null)  //provjerava da li je uopste unesena validna pozcicija te da li na poziciji ima figura
             throw new IllegalArgumentException("Nema figure na poziciji " + oldPosition);
-        if (checkPosition(newPosition) && checkPathToNewPosition(oldPosition, newPosition)) {
+        if(fields[i][j] instanceof Pawn){
+            if(fields[k][l] != null && fields[k][l].getColor() != fields[i][j].getColor()) {
+                fields[i][j].move(newPosition);
+                fields[k][l] = fields[i][j];
+                fields[i][j] = null;
+            }
+        }
+        else if(fields[i][j] instanceof Knight){
+            if(fields[k][l] == null || fields[k][l].getColor() != fields[i][j].getColor()){
+                fields[i][j].move(newPosition);
+                fields[k][l] = fields[i][j];
+                fields[i][j] = null;
+            }
+        }
+        else if (checkPathToNewPosition(oldPosition, newPosition)) {
             fields[i][j].move(newPosition);
-            int [] a = getMatrixPosition(newPosition);
-            fields[a[0]][a[1]] = fields[i][j];
+            fields[k][l] = fields[i][j];
             fields[i][j] = null;
         }
     }
@@ -90,9 +105,9 @@ public class Board {
     public boolean isCheck(ChessPiece.Color color){
         int i,j;
         String kingPosition = new String();
-        if(color == ChessPiece.Color.Black)
+        if(color == ChessPiece.Color.BLACK)
             kingPosition = blackKing.getPosition();
-        else if(color == ChessPiece.Color.White)
+        else if(color == ChessPiece.Color.WHITE)
             kingPosition = whiteKing.getPosition();
         for(i = 0; i < 8; i++){
             for(j = 0; j < 8; j++){
