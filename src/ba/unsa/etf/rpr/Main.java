801d8ffbb7e36352aac065949e82f1851678f1e8
Amer Klinic <aklinic1@etf.unsa.ba>
2019-11-12 23:17:26 +0100

    klasa main prva verzija

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -1,11 +1,52 @@
 package ba.unsa.etf.rpr;
 
+import java.util.Scanner;
+
+import static ba.unsa.etf.rpr.ChessPiece.Color.BLACK;
+import static ba.unsa.etf.rpr.ChessPiece.Color.WHITE;
+
 public class Main {
      public static void main(String[] args) {
-    	//promijeniti sve metode koje nisu defiinisane u tekstu zadatka na private
+         Board chessGame = new Board();
+         int br=0;
+    	while(true){
+            ChessPiece.Color chessPieceColor = null;
+    	    if(br%2 == 0){
+                chessPieceColor = WHITE;
+                System.out.println("White move: ");
+            }
+    	    else if(br%2 != 0){
+    	        chessPieceColor = BLACK;
+                System.out.println("Black move: ");
+            }
+            Scanner scanner = new Scanner(System.in);
+            String pozicija = scanner.nextLine();
+            if(pozicija == "X") {
+                System.out.println("Igrac sa bojom " + chessPieceColor.getColorVal() + " je izgubio partiju!" );
+                break;
+            }
 
-         //prepraviti metode da mogu raditi i sa velikim slovima
+            Class type;
 
-        //dodati u board posebnu provjeru za pijune kada jedu
+            if(pozicija.charAt(0) == 'K'){ type = King.class; }
+            else if(pozicija.charAt(0) == 'Q'){ type = Queen.class;}
+            else if(pozicija.charAt(0) == 'R'){ type = Rook.class;}
+            else if(pozicija.charAt(0) == 'B'){ type = Bishop.class;}
+            else if(pozicija.charAt(0) == 'N'){ type = Knight.class; }
+            else if(pozicija.charAt(0) == 'P'){ type = Pawn.class; }
+            else continue;      //throw IllegalArgumentException("Figura na slovo " + pozicija.charAt(0) + " ne postoji");
+            try {
+                chessGame.move(type, chessPieceColor, pozicija);
+                if (chessPieceColor == WHITE) {
+                    if (chessGame.isCheck(BLACK)) System.out.println("CHECK!!");
+                } else {
+                    if (chessGame.isCheck(WHITE)) System.out.println("CHECK!!");
+                }
+                br++;
+            }
+            catch (Exception e){
+                e.getMessage();
+            }
+        }
     }
 }
